224d3588418e15c8e878ed9e88b8ba7500ca39f9
Lucasfi <lukafigueroa64@gmail.com>
2025-06-24 20:31:28 -0300

    agregamos estilo a los label del form

--- src/components/CardCitas.jsx
+++ src/components/CardCitas.jsx
@@ -12,17 +12,17 @@ const CardCitas = ({ cita }) => {
           </div>
         <div className="card-body bg-info-subtle">
           <p className="card-text">
-            <strong>Fecha: {cita.fecha}</strong>
+            Hora: <strong>{cita.fecha}</strong>
           </p>
           <p className="card-text">
-            <strong>Hora: {cita.hora}</strong>
+            Fecha: <strong>{cita.hora}</strong>
           </p>
           <p className="card-text">
-            <strong>Sintomas: {cita.sintoma}</strong>
+            Sintomas: <strong>{cita.sintoma}</strong>
           </p>
         </div>
         <div className="card-footer bg-secondary text-end">
-          <button className="bg-danger text-white">
+          <button className="botonEliminar">
             Borrar
           </button>
         </div>
